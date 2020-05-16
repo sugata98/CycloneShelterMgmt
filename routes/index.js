5a7c988a21841754cc0be917299aafa61d9e91ad
Sugata Roy <sugata98@gmail.com>
2020-05-16 13:34:58 +0530

    Added User Profile Functionality

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var User = require('../models/user');
+var Campground = require('../models/campground');
 
 //root route
 router.get('/', function(req, res) {
@@ -15,7 +16,13 @@ router.get('/register', function(req, res) {
 
 //handle sign up logic
 router.post('/register', function(req, res) {
-	var newUser = new User({ username: req.body.username });
+	var newUser = new User({
+		username: req.body.username,
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		email: req.body.email,
+		avatar: req.body.avatar
+	});
 	if (req.body.adminCode === 'secretcode123') {
 		newUser.isAdmin = true;
 	}
@@ -58,6 +65,23 @@ router.get('/logout', function(req, res) {
 	req.logout();
 	req.flash('success', 'Logged you out!');
 	res.redirect('/campgrounds');
+});
+
+//USER Profile Route
+router.get('/users/:id', function(req, res) {
+	User.findById(req.params.id, function(err, foundUser) {
+		if (err) {
+			req.flash('error', 'Something went wrong');
+			return res.redirect('/');
+		}
+		Campground.find().where('author.id').equals(foundUser._id).exec(function(err, campgrounds) {
+			if (err) {
+				req.flash('error', 'Something went wrong');
+				return res.redirect('/');
+			}
+			res.render('users/show', { user: foundUser, campgrounds: campgrounds });
+		});
+	});
 });
 
 module.exports = router;
